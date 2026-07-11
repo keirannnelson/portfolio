8a4e843d735570d70c5ac7fd46ba7dd4ecd0a1bc
keiran <keirannnelson@gmail.com>
2026-07-11 16:23:36 -0500

    Add routing and skeleton pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './modules/pages/home/home';
+import { About } from './modules/pages/about/about';
+import { NotFound } from './modules/pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: Home, pathMatch: 'full', title: 'Keiran Nelson' },
+    { path: 'about', component: About, title: 'About - Keiran Nelson' },
+    //{ path: 'posts', component: Posts, title: 'Posts - Keiran Nelson' },
+    //{ path: 'posts/:filename', component: Post },
+    { path: '**', component: NotFound, title: '404 Not Found - Keiran Nelson' } // Wildcard route for 404 pages
+];
